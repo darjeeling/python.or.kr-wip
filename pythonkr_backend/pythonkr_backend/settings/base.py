9778575f00a2dd16fccafadccb79917f5aefb936
Bae KwonHan <darjeeling@gmail.com>
2025-05-31 13:45:01 +0900

    remove logfire from base

--- pythonkr_backend/pythonkr_backend/settings/base.py
+++ pythonkr_backend/pythonkr_backend/settings/base.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import logfire
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -183,7 +182,7 @@ INTERNAL_IPS = [
     "0.0.0.0",
 ]
 
+# import logfire
 # setup logfire
-
-logfire.configure(environment='base', service_name="web")
-logfire.instrument_django()
+#logfire.configure(environment='base', service_name="web")
+#logfire.instrument_django()
