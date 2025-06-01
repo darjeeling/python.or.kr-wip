31aeb9074ca60208149da06999e061c7c3740b4d
Bae KwonHan <darjeeling@gmail.com>
2025-06-01 17:36:03 +0900

    add import

--- pythonkr_backend/pythonkr_backend/__init__.py
+++ pythonkr_backend/pythonkr_backend/__init__.py
@@ -2,6 +2,7 @@
 # Django starts so that shared_task will use this app.
 from django.conf import settings
 
+# load when PROD environment
 if settings.CELERY_ALWAYS_EAGER is False:
     from .celery import app as celery_app
     __all__ = ('celery_app',)
