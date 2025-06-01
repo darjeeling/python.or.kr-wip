ef76df6929bdafd17a3db1a0267d61ffd8e61ad6
Bae KwonHan <darjeeling@gmail.com>
2025-06-01 17:52:20 +0900

    fix import

--- pythonkr_backend/pythonkr_backend/__init__.py
+++ pythonkr_backend/pythonkr_backend/__init__.py
@@ -1,9 +1,10 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from django.conf import settings
+import os
 
-# load when PROD environment
-if settings.CELERY_ALWAYS_EAGER is False:
+DJANGO_SETTINGS_MODULE=os.environ.get("DJANGO_SETTINGS_MODULE")
+
+if "prod" in DJANGO_SETTINGS_MODULE:
     from .celery import app as celery_app
     __all__ = ('celery_app',)
 else:
